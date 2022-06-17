a1756670a1dc4d3ea166194dab6a162a0e135578
jhonywalkeer <mak.jhon@hotmail.com>
2022-06-17 03:39:19 -0300

    test: ensure SignUpController return an error if no name is provided

--- node4devs/src/presentation/controller/signup.spec.ts
+++ node4devs/src/presentation/controller/signup.spec.ts
@@ -1,4 +1,4 @@
-import { SignUpController } from './signup'
+import { SignUpController } from './signup';
 
 describe('SignUp Controller', () => {
   test('Should return 400 if no name is provided', () => {
@@ -7,10 +7,11 @@ describe('SignUp Controller', () => {
       body: {
         email: 'any_email@mail.com',
         password: 'any_password',
-        passwordConfirmation: 'any_password'
-      }
-    }
-    const httpReponse = sut.handle(httpRequest)
-    expect(httpReponse.statusCode).toBe(400)
-  })
-})
+        passwordConfirmation: 'any_password',
+      },
+    };
+    const httpReponse = sut.handle(httpRequest);
+    expect(httpReponse.statusCode).toBe(400);
+    expect(httpReponse.body).toEqual(new Error('Missing parm: name'));
+  });
+});
